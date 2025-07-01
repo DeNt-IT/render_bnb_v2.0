a434ef47f19a00b102bde03b29437c288cec5f9e
Yehor DeNt-IT Maymeskule <dent.trade.acc@gmail.com>
2025-07-01 16:57:15 +0300

    rold

--- render-bnb/src/components/DeNt/GuestPage/GMain/GMainComps/GLeftMain.js
+++ render-bnb/src/components/DeNt/GuestPage/GMain/GMainComps/GLeftMain.js
@@ -3,23 +3,12 @@ import pfp from "../../../../../assets/imgs/DeNt/GuestPage/Ellipse682.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { fetchCurrentUser } from "../../../../../services/currentUserService";
 
 function GLeftMain() {
 
     const navigate = useNavigate();
-    const [profilePic, setProfilePic] = useState(null);
-
-    useEffect(() => {
-        fetchCurrentUser()
-            .then(u => {
-                if (u && u.profilePictureBase64) {
-                    setProfilePic(u.profilePictureBase64);
-                }
-            })
-            .catch(() => {});
-    }, []);
 
     function handleClick(event) {
 
@@ -32,7 +21,7 @@ function GLeftMain() {
             <div className="left-main-top-section-container">
                 <div className="left-main-top-section-box">
                     <div className="left-main-top-section-pfp-container">
-                        <img src={profilePic || pfp} alt='pfp'/>
+                        <img src={pfp} alt = 'pfp'/>
                     </div>
                     <div className="left-main-top-section-name-container">
                         <div className="left-main-top-section-name-text">
