dee31ed64cd3631b2151490151a26ca2ab18435f
Yehor Maymeskule <dent.trade.acc@gmail.com>
2025-07-01 16:26:13 +0300

    Add dynamic profile pictures and logout

--- render-bnb/src/components/DeNt/GuestPage/GHeader/GHeaderComps/Profile.js
+++ render-bnb/src/components/DeNt/GuestPage/GHeader/GHeaderComps/Profile.js
@@ -1,19 +1,44 @@
-import "../../../../../css/DeNt/GuestPage/GuestPage.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { useNavigate } from "react-router-dom";
+import "../../../../../css/DeNt/GuestPage/GuestPage.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { logout } from '../../../../../services/authService';
+import { fetchCurrentUser } from '../../../../../services/currentUserService';
+
 function Profile() {
+    const navigate = useNavigate();
+    const [profilePic, setProfilePic] = useState(null);
 
-    return(
+    useEffect(() => {
+        fetchCurrentUser()
+            .then(u => {
+                if (u && u.profilePictureBase64) {
+                    setProfilePic(u.profilePictureBase64);
+                }
+            })
+            .catch(() => {});
+    }, []);
 
-        <div className = 'profile-wrapper'>
-            <div className = 'profile-burger-container'>
+    function handleLogout() {
+        logout();
+        navigate('/');
+    }
+
+    return (
+        <div className='profile-wrapper'>
+            <div className='profile-burger-container'>
                 <FontAwesomeIcon icon={faBars} />
             </div>
-            <div className = 'profile-user-container'>
-                <FontAwesomeIcon icon={faUser} />
+            <div className='profile-user-container'>
+                {profilePic ? (
+                    <img src={profilePic} alt='User' className='irina-user' />
+                ) : (
+                    <FontAwesomeIcon icon={faUser} />
+                )}
             </div>
+            <button className='profile-logout-btn' onClick={handleLogout}>Log Out</button>
         </div>
     );
 }
